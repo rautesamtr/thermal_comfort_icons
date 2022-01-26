a4d4a396f66b868b7b6b5d0894eab114c9008c0e
Gabriel Rauter <rauter.gabriel@gmail.com>
2022-01-26 17:24:31 +0100

    Update generate_js script for backwards compat

--- generate_js.py
+++ generate_js.py
@@ -3,18 +3,41 @@ import json
 import os
 from glob import glob
 from string import Template
-from xml.dom.minidom import parse
+from xml.dom.minidom import Document, parse
 
 SVG = "svg"
 OUTFILE = os.path.join("dist", "thermal_comfort_icons.js")
 
+
+def get_path(dom: Document) -> str:
+    """Get the path of the svg file."""
+    return dom.getElementsByTagName("path")[0].getAttribute("d")
+
+
+def get_id(dom: Document) -> str:
+    """Get the id of the svg file."""
+    return dom.getElementsByTagName(SVG)[0].getAttribute("id")
+
+
+def get_keywords(dom: Document) -> str:
+    """Get the keywords of the svg file."""
+    desc = dom.getElementsByTagName("desc")[0]
+    if desc.firstChild is not None:
+        return desc.firstChild.nodeValue
+    else:
+        return []
+
+
+doms = [parse(file) for file in glob(os.path.join(SVG, f"*.{SVG}"))]
+
 icons = {
-    dom.getElementsByTagName("svg")[0].getAttribute("id"): {
-        "path": dom.getElementsByTagName("path")[0].getAttribute("d"),
-        "keywords": dom.getElementsByTagName("desc")[0].firstChild.nodeValue.split(" "),
+    get_id(dom): {
+        "path": get_path(dom),
+        "keywords": get_keywords(dom),
     }
-    for dom in [parse(file) for file in glob(os.path.join(SVG, f"*.{SVG}"))]
+    for dom in doms
 }
+
 template = Template(
     """const TC_ICONS_MAP = $icons;
 
@@ -29,7 +52,11 @@ async function getIconList() {
 }
 
 window.customIcons = window.customIcons || {};
-window.customIcons["tc"] = { getIcon, getIconList };"""
+window.customIcons["tc"] = { getIcon, getIconList };
+
+window.customIconsets = window.customIconsets || {};
+window.customIconsets["bha"] = getIcon;
+"""
 )
 
 js = template.substitute(icons=json.dumps(icons, sort_keys=True, indent=2))
